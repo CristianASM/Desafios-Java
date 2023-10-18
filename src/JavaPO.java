419845e58533798efdf1db997dda504c407f5560
Cristian Soto <cristiansoto.m10@gmail.com>
2023-10-17 22:13:59 -0300

    level up

---
+++ src/JavaPO.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class JavaPO {
+    /**Cuenta la:   -Cantidad de palabras de una oracion
+                    -La palabra mas larga
+                    -Invierte la oracion
+                    -Invierte todo.*/
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String oracion;
+
+        System.out.print("Ingrese una oracion: ");
+        oracion = teclado.nextLine();
+
+        String[] palabras = oracion.split(" ");
+        int cantidadPalabras = palabras.length;
+
+        String palabraMasLarga = "";
+        for (String palabra : palabras) {
+            if (palabra.length() > palabraMasLarga.length()) {
+                palabraMasLarga = palabra;
+            }
+        }
+
+        StringBuilder oracionInvertida = new StringBuilder();
+        for (int i = palabras.length - 1; i >= 0; i--) {
+            oracionInvertida.append(palabras[i]).append(" ");
+        }
+        /*String oracionInvertida2 = "";
+        for (int i = palabras.length - 1; i >= 0; i--) {
+            oracionInvertida2 += palabras[i] + " ";
+        }*/
+
+        String[] palabrasInvertidas = new String[palabras.length];
+        for(int i=0;i<palabras.length;i++){
+            palabrasInvertidas[i] = new StringBuilder(palabras[i]).reverse().toString();
+        }
+
+
+        System.out.println("Cantidad de palabras: "+cantidadPalabras);
+        System.out.println("Palabra mas larga: "+ palabraMasLarga);
+        System.out.println("Oracion invertida: "+ oracionInvertida);
+        System.out.println("Todo invertido: "+  String.join(" ", palabrasInvertidas));
+
+    }
+}
