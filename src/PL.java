fb689b82a3619de5bb0dfee4bb828df283b6a6b0
Cristian Soto <cristiansoto.m10@gmail.com>
2023-10-31 22:02:36 -0300

    level up

---
+++ src/PL.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PL {
+    // Detector de palindromo
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String palabra;
+        StringBuilder palabraInvertida = new StringBuilder();
+        char caracter;
+
+        System.out.print("Ingrese una palabra: ");
+        palabra = teclado.next().toLowerCase();
+
+        for (int i = palabra.length() - 1;i>=0;i--){
+            caracter = palabra.charAt(i);
+            palabraInvertida.append(caracter);
+        }
+
+        if (palabra.equals(palabraInvertida.toString())){
+            System.out.println("La palabra "+palabra+" SI es un palindromo");
+        }else {
+            System.out.println("La palabra "+palabra+" NO es un palindromo");
+        }
+    }
+}
